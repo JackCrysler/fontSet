b0abe0d4f88c9bf7950fd7353210c421e87c7786
JackCrysler <jack_crysler@163.com>
2016-11-21 16:42:32 +0800

    first commit

---
+++ setfontsize.js
@@ -0,0 +1,15 @@
+﻿(function (doc, win) {
+    var docEl = doc.documentElement, //获取html标签
+		//orientationchange方向改变事件
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+			//当前设备视口宽度
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
